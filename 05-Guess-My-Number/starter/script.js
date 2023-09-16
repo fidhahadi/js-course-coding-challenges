e6cf5718b61acf8caebc6f8cb3570e005852e52d
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-16 15:19:59 +0400

    manipulating css

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -24,6 +24,8 @@ document.querySelector('.check').addEventListener('click', function () {
     }
     else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct number';
+        document.querySelector('body').style.backgroundColor = `#60b347`;
+        document.querySelector('.number').style.width = '30rem';
     }
     else if (guess < secretNumber) {
 
