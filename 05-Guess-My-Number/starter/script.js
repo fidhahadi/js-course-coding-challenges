3b2c65a7304cc9cd73db02321e7ada790d460748
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-09-19 11:40:47 +0400

    guess the number completed

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,8 +11,10 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 24;
 console.log(document.querySelector('.guess').value); 
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+document.querySelector('.number').textContent = secretNumber;
 
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -26,7 +28,10 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = `#60b347`;
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
-
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
     }
     else if (guess < secretNumber) {
 
@@ -52,4 +57,15 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start Guessing....';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = " ";
+    // document.querySelector('.number').textContent = "?";
+    document.querySelector('body').style.backgroundColor = `#222`;
+    document.querySelector('.number').style.width = '15rem';
 });
